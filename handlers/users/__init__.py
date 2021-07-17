4072d776d02071692a6c2893105fe9c62b854343
ob1vanq <69727591+ob1vanq@users.noreply.github.com>
2021-07-17 12:59:43 +0300

    work with ReplyKeyboardMarkup

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -2,7 +2,7 @@ from .inline_mode import dp
 from .start import dp
 from .help import dp
 #from .state import dp
-#from .menu import dp
+from .menu import dp
 from .is_true import dp
 from .usemy_item_test import dp
 #from .button1 import dp
--- keyboards/default/menu.py
+++ keyboards/default/menu.py
@@ -1,15 +1,27 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from tkinter import Tk
+
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, KeyboardButtonPollType
 
 menu = ReplyKeyboardMarkup(
     keyboard=[
         [
+            KeyboardButton(text="Указать свой номер", request_contact= True),
+            KeyboardButton(text="Указать свою локацию", request_location = True)
+        ],
+        [
             KeyboardButton(text="Что-либо"),
             KeyboardButton(text="Нечто другое")
         ],
+
         [
             KeyboardButton(text="Отмена")
 
+        ],
+        [
+            KeyboardButton(text="Создать опрос", request_poll=KeyboardButtonPollType())
         ]
+
     ],
-    resize_keyboard=True
+    resize_keyboard=True,
+    selective=True
 )
